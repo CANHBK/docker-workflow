ed882798874eb5b2f746d518a8f80c1cda8f93a4
CANHBK <canhcvp198@gmail.com>
2019-03-31 15:20:14 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Xin chao canh
           </a>
         </header>
       </div>
